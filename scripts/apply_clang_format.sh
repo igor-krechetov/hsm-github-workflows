8f8c69da4d8432473f0f4279efeea123a0c39e09
igor-krechetov <58586005+igor-krechetov@users.noreply.github.com>
2025-11-29 13:40:30 +0000

    Draft for HSM reusable github workflows

---
+++ scripts/apply_clang_format.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+find ./src/ -iname *.hpp -o -iname *.cpp | xargs clang-format -i
+find ./examples/ -iname *.hpp -o -iname *.cpp | xargs clang-format -i
+find ./tests/ -iname *.hpp -o -iname *.cpp | xargs clang-format -i
+find ./tests/ -iname *.h -o -iname *.c | xargs clang-format -i
---
+++ scripts/build_vs.cmd
@@ -0,0 +1,15 @@
+mkdir .\build
+cd .\build
+
+@rem Fix path according to your system
+@rem For VS 2019
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+@rem For VS 2015
+@rem call "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+
+@rem Change Qt5_DIR to match your system if you want to enable Qt dispatcher
+@rem set Qt5_DIR=C:\Qt\5.9\5.9.2\msvc2015\lib\cmake\Qt5
+@rem cmake -DHSMBUILD_DISPATCHER_GLIB=OFF -DHSMBUILD_DISPATCHER_GLIBMM=OFF ..
+cmake -DHSMBUILD_DISPATCHER_GLIB=OFF -DHSMBUILD_DISPATCHER_GLIBMM=OFF -DHSMBUILD_DISPATCHER_QT=OFF ..
+MSBuild.exe hsmcpp.sln -target:ALL_BUILD
+pause
